529e1c5b46a9897a28737f61d04517a56c8ce286
Dimitris Dimos <dimitris.dimos647@gmail.com>
2022-09-10 03:33:21 +0300

    minor change

---
+++ make_grid.py
@@ -0,0 +1,66 @@
+import argparse
+import os
+import sys
+
+import cv2
+import numpy as np
+import torch
+from skimage.metrics import peak_signal_noise_ratio as psnr
+from skimage.metrics import structural_similarity as ssim
+from torchvision import transforms
+from torchvision.io import read_image
+from torchvision.utils import make_grid
+
+
+def edit(orig_img, recon_img):
+
+        recon_img[recon_img <= 0.05 * torch.max(orig_img)] = 0
+        orig_img[orig_img <= 0.05 * torch.max(orig_img)] = 0
+
+        orig_np = orig_img.squeeze().cpu().numpy()
+        orig_np *= 255.0 / orig_np.max()
+        orig_np = orig_np.astype(np.uint8)
+        recon_np = recon_img.squeeze().cpu().numpy()
+        recon_np *= 255.0 / recon_np.max()
+        recon_np = recon_np.astype(np.uint8)
+
+        mask = np.zeros(recon_np.shape, dtype=np.uint8)
+        cv2.circle(mask, (192, 192), 165, 255, -1)
+        recon_np = cv2.bitwise_and(recon_np, recon_np, mask=mask)
+
+        return orig_img, recon_img, orig_np, recon_np
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Template')
+    parser.add_argument('--exp_dir', type=str, required=True, help='Path to experiment images')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = get_args()
+
+    to_plot = []
+    for i in range(16):
+        origi = read_image(os.path.join(args.exp_dir, f'brain_T2_{i}_or.jpg'))
+        recon = read_image(os.path.join(args.exp_dir, f'brain_T2_{i}.jpg'))
+
+        _, _, orig_np, recon_np = edit(origi, recon)
+
+        ssim_score = ssim(orig_np, recon_np)
+        psnr_score = psnr(orig_np, recon_np)
+
+        to_plot.append( origi )
+        to_plot.append( recon )
+
+
+    grid = make_grid(to_plot, nrow=2, ncol=16, padding=10)
+    grid = transforms.ToPILImage()(grid)
+    grid.save( os.path.join(args.exp_dir, f'brains_32_vertical.jpg') )
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
